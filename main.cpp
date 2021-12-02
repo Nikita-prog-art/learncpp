72a1ecfb3aebba901cb30e0e35288f0ddf44f2e1
Nikita-prog-art <xanext@mail.ru>
2021-12-02 17:50:16 +0500

    mass - 6 - 1

--- main.cpp
+++ main.cpp
@@ -3,5 +3,18 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!";
+    int n;
+    cin >> n;
+    int mass[20];
+    for (int i = 0; i < n; i++){
+        cin >> mass[i];
+    }
+    for (int i = 0; i < n - 1; i += 2){
+        int buff = mass[i];
+        mass[i] = mass[i + 1];
+        mass[i + 1] = buff;
+    }
+    for (int i = 0; i < n; i++){
+        cout << mass[i];
+    }
 }
