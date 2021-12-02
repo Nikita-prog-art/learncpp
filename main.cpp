9116bc1746adcd24851611c559784377b011ab25
Nikita-prog-art <xanext@mail.ru>
2021-12-02 20:49:25 +0500

    mass - 6 - 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
-    int mass[20];
+    int n, a, b;
+    cin >> n >> a >> b;
+    a--;
+    b--;
+    vector <int> mass(n);
     for (int i = 0; i < n; i++){
         cin >> mass[i];
     }
-    for (int i = 0; i < n - 1; i += 2){
+    for (int i = a; i < (b + a) / 2; i++){
         int buff = mass[i];
-        mass[i] = mass[i + 1];
-        mass[i + 1] = buff;
+        mass[i] = mass[b - i + a];
+        mass[b - i + a] = buff;
     }
     for (int i = 0; i < n; i++){
         cout << mass[i] << " ";
