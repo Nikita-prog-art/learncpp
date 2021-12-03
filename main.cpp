3030fa5582c55961893497aa293044b4afd20c4a
Nikita-prog-art <xanext@mail.ru>
2021-12-03 17:01:50 +0500

    mass - 6 - 3

--- main.cpp
+++ main.cpp
@@ -4,20 +4,23 @@
 using namespace std;
 
 int main(){
-    int n, a, b;
-    cin >> n >> a >> b;
-    a--;
-    b--;
+    int n;
+    cin >> n;
     vector <int> mass(n);
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
         cin >> mass[i];
-    }
-    for (int i = a; i < (b + a) / 2; i++){
-        int buff = mass[i];
-        mass[i] = mass[b - i + a];
-        mass[b - i + a] = buff;
-    }
-    for (int i = 0; i < n; i++){
+    int r;
+    cin >> r;
+    cout << "before: ";
+    for (int i = 0; i < n; i++)
         cout << mass[i] << " ";
+    vector <int> newMass(n);
+    for (int i = 0; i < n; i++){
+        int ri = (r + i) % n;
+        newMass[ri] = mass[i];
     }
+    cout << "\n";
+    cout << "after: ";
+    for (int i = 0; i < n; i++)
+        cout << newMass[i] << " ";
 }
