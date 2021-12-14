eb40ddfd6bfb9a6a3f739dcb3139288be19fa64d
Nikitaprogartlocal <xanext@mail.ru>
2021-12-14 19:23:16 +0500

    ^2sort - 2

--- main.cpp
+++ main.cpp
@@ -9,16 +9,17 @@ int main(){
     vector <int> mass(n);
     for (int i = 0; i < n; i++)
         cin >> mass[i];
+    long long cnt = 0;
     for (int i = 0; i < n - 1; i++){
-        bool sorted = true; //обмена нет
+        bool sd = true; //обмена нет
         for (int j = 0; j < n - 1 - i; j++)
             if (mass[j] > mass[j + 1]){
                 swap(mass[j], mass[j + 1]);
-                sorted = false; //обмен есть!
+                sd = false; //обмен есть!
+                cnt++;
             }
-        if (sorted)
+        if (sd)
             break;
     }
-    for (auto x : mass)
-        cout << x << " ";
+    cout << cnt;
 }
